c025cbc79f6d8f64314c80657490774efd4320ba
DESKTOP-OKM9V82\Admin <tpnghi.project@gmail.com>
2021-05-24 16:00:37 +0700

    remove file seletor

--- src/actions/books.js
+++ src/actions/books.js
@@ -2,39 +2,41 @@ import {
   getStoragePermission,
   checkStoragePermissions,
 } from '../utils/permissions';
-import showToast from "../components/Toast";
+import showToast from '../components/Toast';
 //import DocumentPicker from 'react-native-document-picker';
-import RNFileSelector from 'react-native-file-selector';
+// import RNFileSelector from 'react-native-file-selector';
 
-export const addBook = ({ navigation, fileUrl }) => async (dispatch) => {
+export const addBook = ({ navigation, fileUrl }) => async dispatch => {
   let granted = await checkStoragePermissions();
   if (!granted) await getStoragePermission();
 
   const _addBook = url => {
-    let components = url.split("/");
-    let file = components[components.length - 1].split(".");
-    let title = file[0]
-    let type = file[file.length - 1].toLowerCase()
+    let components = url.split('/');
+    let file = components[components.length - 1].split('.');
+    let title = file[0];
+    let type = file[file.length - 1].toLowerCase();
     dispatch({
-      type: "add_books",
+      type: 'add_books',
       payload: { title, url, type },
     });
-    navigation.navigate(`${type || 'epub' || 'pdf'}-reader`, { title, url, index: 0, });
-  }
+    navigation.navigate(`${type || 'epub' || 'pdf'}-reader`, {
+      title,
+      url,
+      index: 0,
+    });
+  };
 
   if (fileUrl === undefined) {
-
-    RNFileSelector.Show({
-      title: "Select ebook file",
-      filter: ".*\\.(epub|EPUB|pdf|PDF)$",
-
-      onDone: (url) => {
-        _addBook(url);
-      },
-      onCancel: () => {
-        showToast("Cancel open book");
-      },
-    });
+    // RNFileSelector.Show({
+    //   title: "Select ebook file",
+    //   filter: ".*\\.(epub|EPUB|pdf|PDF)$",
+    //   onDone: (url) => {
+    //     _addBook(url);
+    //   },
+    //   onCancel: () => {
+    //     showToast("Cancel open book");
+    //   },
+    // });
   } else {
     _addBook(fileUrl);
   }
